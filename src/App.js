6c2b3dfa7c8c0e44192ce0965ec4176ccc3fddac
olympiawoj <owojcik7@gmail.com>
2021-03-11 10:27:49 -0500

    Creating Timer Forms

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 export default function App() {
+  const [timers, setTimers] = useState([{ time: 2, text: 'this is my message' },
+  { time: 5, text: 'hello' },
+  { time: 8, text: 'whats up' }])
+
+  function updateTimers(index, time, text){
+    const newTimers=[...timers]
+    newTimers[index].time = time;
+    newTimers[index].text = text;
+    setTimers(newTimers)
+  }
+
+  function addTimer(){
+    const newTimers = [...timers, {time: 100, text: 'yooo'}]
+    setTimers(newTimers)
+  }
+
   return (
     <div className="app">
       <h2>Talk the Talk</h2>
 
       <div className="timers">
-        {/* timers go here */}
-        <form className="timer">
-          <input type="number" />
-          <input type="text" />
-        </form>
-
-        <button className="add-button">Add</button>
+        {timers.map((timer, index) => (
+          <TimerSlot key={index} index={index} timer={timer} updateTimers={updateTimers}/>
+        ))}
+        <button className="add-button" onClick={addTimer}>Add</button>
       </div>
 
       {/* seconds */}
@@ -26,4 +39,20 @@ export default function App() {
       </div>
     </div>
   );
+}
+
+function TimerSlot({index, timer, updateTimers}){
+  const [time, setTime] = useState(timer.time)
+  const [text, setText] = useState(timer.text)
+
+  function handleBlur(){
+    updateTimers(index, time, text)
+  }
+
+  return (
+    <form className="timer" key={index}>
+    <input type="number" value={time} onChange={(e)=> setTime(e.target.value)} onBlur={handleBlur}/>
+    <input type="text" value={text} onChange={(e) => setText(e.target.value)} onBlur={handleBlur}/>
+  </form>
+  )
 }
